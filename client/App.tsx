eb654d5f0845504caa0407152d4c4b79ea29ba32
서호준 <henryseo1000@gmail.com>
2023-07-21 21:41:01 +0900

    splash 스크린 추가, 수정, 앱 아이콘 수정

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {Component, useEffect} from 'react';
 import TopBar from './src/components/TopBar';
 import BottomBar from './src/components/BottomBar';
 import Content from './src/components/Content';
 import { SafeAreaView,ScrollView,StyleSheet,Text,View } from 'react-native';
 import { MD2Colors as Colors, Menu } from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App(){
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
     <SafeAreaView style={styles.flex}>
--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,36 @@
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
-import {useQuery} from "react-query";
-import profile from './images/profile.png';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
-import Login from './login';
+import React, {useEffect} from 'react';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import rootReducer from './src/modules';
+import {NavigationContainer} from '@react-navigation/native';
+import StackNav from './src/navigations/Stack';
+import {shoppingListApi} from './src/lib/api/shoppingList';
+import SplashScreen from 'react-native-splash-screen';	// 라이브러리 추가!!
 
-function App() {
-    const [message, setMessage] = useState("");
-    const [getlamp, setlamp] = useState("");
-    const Url = "http://localhost:8080";
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: shoppingListApi,
+      },
+      serializableCheck: false,
+    }),
+});
 
-    useEffect(() => {
-              fetch('/login')
-                  .then(response => response.text())
-                  .then(message => {
-                      setMessage(message);
-                  });
-    },[])
-
-    return (
-    <div className="App">
-        <div className="top-nav">
-            <div className="title">
-                <h1>LH Project</h1>
-            </div>
-            <div className="login-profile">
-                <img className="profile" src = {profile}>
-                </img>
-                <a href="/login">로그인 필요</a>
-            </div>
-        </div>
-        <div className="contentWrapper">
-            <div className="set-bar">
-                <div className = "set-temp">
-                    <input type="text" size="10"></input>
-                    <button>온도 입력</button>
-                </div>
-                <div className = "set-humid">
-                    <input type="text" size="10"></input>
-                    <button>습도 입력</button>
-                </div>
-                <div className = "set-water">
-                    <input type="time" size="10"></input>
-                    <button>분무 시간 입력</button>
-                </div>
-                <div className = "set-lamp">
-                    <input type="time" size="10"></input>
-                    <button>조명 시간 입력</button>
-                </div>
-                <div className = "test_data">
-                    <button onClick={() => {axios.get("http://localhost:3001/api/user")
-                    .then((res: any) => {
-                        console.log(res);
-                    }).catch((err: any) => {
-                        console.log(err);
-                    })
-                    }}>데이터 가져오기</button>
-                </div>
-            </div>
-        </div>
-        <div className="bottom-nav">
-       <h4 dangerouslySetInnerHTML={{__html:message}}></h4>
-            <a href = "localhost:8080/login">로그인</a>
-            <text>Copyrights reserved by HojunSeo</text>
-        </div>
-    </div>
-    );
-}
+const App = () => {
+	// SplachScreen hide 하는 코드 추가한다!!!!!
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <StackNav />
+      </NavigationContainer>
+    </Provider>
+  );
+};
 
 export default App;
